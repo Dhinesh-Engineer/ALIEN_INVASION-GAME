56dfb1e454a5079706b59cb5bdc1fb38f2baabd5
Praveen-mc <praveensb2002@gmail.com>
2022-01-23 18:40:58 +0530

    Image path updated

--- Game modules/alien.py
+++ Game modules/alien.py
@@ -11,7 +11,7 @@ class Alien(Sprite):
         self.ai_settings = ai_settings
 
         # load the alien image and set its rect attribute.
-        self.image = pygame.image.load('alien.bmp')
+        self.image = pygame.image.load('../images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # start each new alien near the top left of the screen.
